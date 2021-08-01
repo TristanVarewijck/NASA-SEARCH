0e9dc011cc36893151431126cde6d5bb9792ca7a
TristanVarewijck <tristanconnect4@outlook.com>
2021-08-01 17:18:06 +0200

    important layouts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const engine = require('ejs-mate');
 const bodyParser = require('body-parser');
 const colors = require('colors');
-require('dotenv').config();
+require('dotenv').config(); 
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,9 +14,25 @@ app.engine('ejs', engine);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// redirect
 app.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/spacewatch');
   });
+
+// HOME
+app.get('/spacewatch', (req, res) => {
+  res.render('index');
+});
+
+// GET RESPONSE
+app.get('/spacewatch', (req, res) => {
+  res.render('index');
+});
+
+// POST Button clicks
+app.post('/spacewatch', (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => {
     console.log(colors.yellow(`Example app listening at http://localhost:${port}`));
